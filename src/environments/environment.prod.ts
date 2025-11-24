42ae46d25dbfca4e4c249f69aab31376681508e2
xdaniel-nicola <daniel.nicola02@gmail.com>
2025-11-24 20:25:45 -0300

    mudança no firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
-export const environment = {
+const environment = {
   production: true,
   firebaseConfig: {
-  apiKey: "AIzaSyC5KjozTzgE0mxyvi2HzZHGSZDAHyh8pR0",
-  authDomain: "projetobookly.firebaseapp.com",
-  projectId: "projetobookly",
-  storageBucket: "projetobookly.firebasestorage.app",
-  messagingSenderId: "393387365728",
-  appId: "1:393387365728:web:695ccd864808ac6b1d7a05",
-  measurementId: "G-NY4VVZGYLJ"
+  apiKey: "AIzaSyAY2LzY_nxqMXzUjJPKgmiucZY2uAMhI2I",
+  authDomain: "projetobookly-1f8ad.firebaseapp.com",
+  projectId: "projetobookly-1f8ad",
+  storageBucket: "projetobookly-1f8ad.firebasestorage.app",
+  messagingSenderId: "104789560838",
+  appId: "1:104789560838:web:952a9d3d9979ba6c31fd87",
+  measurementId: "G-K8CLXF0Q9W"
   }
 };
